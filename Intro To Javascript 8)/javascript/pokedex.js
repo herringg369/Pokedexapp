6825cb325ec7b83f07afc09342697b4ddf21f307
herringg369 <83850196+herringg369@users.noreply.github.com>
2021-05-20 21:29:50 -0400

    added the files to the repository

---
+++ Intro To Javascript 8)/javascript/pokedex.js
@@ -0,0 +1,23 @@
+let pokeList = {
+			{name:"bulbasaur",
+			height:0.7,
+			type:["grass","poison"]},
+			{name:"charizard",
+			height:1.7,
+			type:["fire","flying"]},
+			{name:"beedrill",
+			height:1,
+			type:["bug","poison"]}
+	};
+
+for (let i=0; i < pokeList.length; i++){
+	document.write(pokeList.name + pokeList.height));
+
+if (pokeList[i].height >= 2){
+document.write(pokeList[i].name + " (" + "height" + pokeList[i].height ") " + "Wow what a large pokemon!");
+}else{
+document.write(pokeList[i].name + " (" + "height" + pokeList[i].height ") ");
+}
+}
+
+alert("this is a test bacon")
